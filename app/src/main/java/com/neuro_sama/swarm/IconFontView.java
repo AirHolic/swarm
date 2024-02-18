30825e0c3226603f3bb901b140bcbd48ced9b46d
AirHolic <2670446215@qq.com>
2024-02-18 17:18:03 +0800

    Initial commit

---
+++ app/src/main/java/com/neuro_sama/swarm/IconFontView.java
@@ -0,0 +1,13 @@
+package com.neuro_sama.swarm;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.widget.TextView;
+
+
+@SuppressLint("AppCompatCustomView")
+public class IconFontView extends TextView {
+    public IconFontView(Context context) {
+        super(context);
+    }
+}
---
+++ app/src/main/java/com/neuro_sama/swarm/MainActivity.java
@@ -0,0 +1,16 @@
+package com.neuro_sama.swarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import static com.hivemq.client.mqtt.MqttGlobalPublishFilter.ALL;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
