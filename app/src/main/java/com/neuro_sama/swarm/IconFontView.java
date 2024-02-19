4ae97905c8afd1c7f95892efcccbb3f0f2d50151
AirHolic <2670446215@qq.com>
2024-02-19 10:55:43 +0800

    添加iconfont类和启动mqtt所需权限

--- app/src/main/java/com/neuro_sama/swarm/IconFontView.java
+++ app/src/main/java/com/neuro_sama/swarm/IconFontView.java
@@ -2,6 +2,8 @@ package com.neuro_sama.swarm;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
 import android.widget.TextView;
 
 
@@ -9,5 +11,20 @@ import android.widget.TextView;
 public class IconFontView extends TextView {
     public IconFontView(Context context) {
         super(context);
+    }
+
+    public IconFontView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public IconFontView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context){
+        Typeface iconfont = Typeface.createFromAsset(context.getAssets(), "iconfont.ttf");
+        setTypeface(iconfont);
     }
 }
